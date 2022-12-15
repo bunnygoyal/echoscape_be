155156370b5d559d571f0fdaade8fd48d6db3b21
daniyaniazii <daniya.niazi@techlogix.com>
2022-12-16 01:46:17 +0500

    fix frontend audio

--- flask/app.py
+++ flask/app.py
@@ -1,3 +1,4 @@
+import os
 import openai
 import whisper
 from flask import Flask, request, request
@@ -8,10 +9,14 @@ openai.api_key = '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' #my key
 
 @app.route('/api/generate_image', methods=['POST'])
 def generate_image():
-    print(request.form.keys())
-    if 'audio_path' not in request.form:
+    print(request.files['audio_path'])
+    if 'audio_path' not in request.files:
         return {'message':'bad request'},401
-    whisper_output = transcribe_audio(base_model,request.form['audio_path'])
+
+    dirname = os.path.dirname("audios")
+    save_path = os.path.join(dirname, "temp.wav")
+    request.files['audio_path'].save(save_path)
+    whisper_output = transcribe_audio(base_model,save_path)
     gpt_output = get_dalle_prompt(whisper_output)
     dalle_output = generate_dalle_image(gpt_output)
     print(gpt_output)
